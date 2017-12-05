0e1b2a4dc8f3842ed6bc0f0d11114ec8c6878c27
AGit <30912338+AdamGitFun@users.noreply.github.com>
2017-12-04 20:42:34 -0500

    extracts javascript into separate file

---
+++ resources/js/parents.js
@@ -0,0 +1,28 @@
+// function to randomly choose what you would be doing as a parent
+
+function getParentAction() {
+
+  var input = document.getElementById("inputText").value;
+
+  if (input == "") {
+    alert("You must enter what you did!");
+
+  } else {
+
+    var result_possibilities = ["Went to Target.",
+    "Gone to Home Depot.",
+    "Brought the kids to the park.",
+    "Gone grocery shopping...again.",
+    "Fought with your partner for no reason, but it was likely catalyzed by having children.",
+    "Argued about who got to run the errands alone.",
+    "Googled how to remove vomit smell from...",
+    "Clipped tiny fingernails.",
+    "Thought about touching your partner but got too tired.",
+    "Stayed at home."];
+
+    var result = result_possibilities[Math.floor(Math.random()*result_possibilities.length)];
+
+    document.getElementById("resultText").innerHTML = result;
+  }
+
+}
